89294751af2e4ab2bf2e24ce4285920002adb71f
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2024-10-05 17:55:01 -0700

    Connected to localDB

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
 require("dotenv").config();
 const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
 
 const app = express();
 const PORT = process.env.PORT || 8080
+const dbUrl = process.env.DB_URL
 
+
+//Connect to Database
+async function connectToMongo() {
+    mongoose.set("strictQuery", false);
+    try {
+        await mongoose.connect(dbUrl);
+        console.log("Successfully connected to MongoDB database");
+        console.log("Connected to database:", mongoose.connection.name); // Logs the database name
+    } catch (err) {
+        console.error("Error connecting to MongoDB:", err);
+    }
+}
+
+
+connectToMongo().catch(err => console.log("Some error"));
+
+//Middlewares
+app.use(cors());
+app.use(express.json());
+
+
+//Routes
 app.get("/", (req, res) => {
     res.status(200).send("This is the response from the Primary Backend");
 })
