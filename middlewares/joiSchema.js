9f3f8de42c66cfe4cf62dedf40a2c1c7be404371
Himanshu-Dangwal <himanshudangwal99@gmail.com>
2025-02-24 20:37:19 -0800

    Added Captch protection

--- middlewares/joiSchema.js
+++ middlewares/joiSchema.js
@@ -3,10 +3,12 @@ const Joi = require('joi');
 module.exports.userSchema = Joi.object({
     username: Joi.string().required().alphanum().min(3).max(25),
     email: Joi.string().required(),
-    password: Joi.string().required().min(5)
+    password: Joi.string().required().min(5),
+    captchaValue: Joi.string().required()
 })
 
 module.exports.loginSchema = Joi.object({
     email: Joi.string().required(),
-    password: Joi.string().required().min(5)
+    password: Joi.string().required().min(5),
+    captchaValue: Joi.string().required()
 })
